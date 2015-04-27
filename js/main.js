ab1e5f844885f591cc21ea2b00fcf8ed08a369ff
Gustavo Teodoro <gustavoalvesteodoro@gmail.com>
2015-04-27 16:21:13 -0300

    Iniciando projeto

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready ( function(){
+   $('body').vegas({
+		animation: 'kenburns',
+   		overlay: true,
+   		delay: 9000,
+   		shuffle: true,
+	    slides: [
+	        { src: 'img/foto1.jpg' },
+	        //{ src: 'img/foto2.jpg' },
+	        { src: 'img/foto3.jpg' },
+	        { src: 'img/foto4.jpg' },
+	        { src: 'img/foto5.jpg' }
+	    ]
+	});
+});
